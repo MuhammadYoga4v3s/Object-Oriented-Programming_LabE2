23ab773c36223269cf723eb36d1b2e6928a7ec8e
Muhammad Yoga Aminudin <muhammadyogavas@gmail.com>
2025-06-23 08:07:56 +0700

    Update LambdaMap.java

--- Pertemuan_11 (Lambda)/EkspresiLAmbda/src/main/java/com/mycompany/ekspresilambda/LambdaMap.java
+++ Pertemuan_11 (Lambda)/EkspresiLAmbda/src/main/java/com/mycompany/ekspresilambda/LambdaMap.java
@@ -1,18 +1,27 @@
+/* NamaFile  : LambdaMap.java
+ * Deskripsi : Program untuk menampilkan isi Map (NIM dan nama mahasiswa) menggunakan ekspresi lambda
+ * Pembuat   : Muhammad Yoga Aminudin (24060123130106)
+ * Tanggal   : 20/05/2025
+ */
+
 package com.mycompany.ekspresilambda;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class LambdaMap {
+    // Fungsi utama untuk menjalankan program
     public static void main(String[] args) {
         // Membuat Map dengan NIM sebagai key dan nama sebagai value
-        Map<String, String> mahasiswaMap = new HashMa p<>();
+        Map<String, String> mahasiswaMap = new HashMap<>();
+
+        // Menambahkan pasangan NIM dan nama ke dalam Map
         mahasiswaMap.put("123", "Adi");
         mahasiswaMap.put("124", "Bambang");
         mahasiswaMap.put("125", "Cici");
         mahasiswaMap.put("126", "Didi");
 
-        // Menampilkan key dan value menggunakan ekspresi lambda
+        // Menampilkan semua pasangan NIM dan nama menggunakan ekspresi lambda
         mahasiswaMap.forEach((nim, nama) -> System.out.println("NIM: " + nim + ", Nama: " + nama));
     }
 }
