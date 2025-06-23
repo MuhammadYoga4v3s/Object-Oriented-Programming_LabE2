d57d7962d1c785d5ef98b9f07e5db384868f7088
Muhammad Yoga Aminudin <muhammadyogavas@gmail.com>
2025-06-23 08:07:21 +0700

    Update LambdaList.java

--- Pertemuan_11 (Lambda)/EkspresiLAmbda/src/main/java/com/mycompany/ekspresilambda/LambdaList.java
+++ Pertemuan_11 (Lambda)/EkspresiLAmbda/src/main/java/com/mycompany/ekspresilambda/LambdaList.java
@@ -1,19 +1,26 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+/* NamaFile  : LambdaList.java
+ * Deskripsi : Program untuk menampilkan daftar nama mahasiswa menggunakan ekspresi lambda
+ * Pembuat   : Muhammad Yoga Aminudin (24060123130106)
+ * Tanggal   : 20/05/2025
  */
+
 package com.mycompany.ekspresilambda;
+
 import java.util.ArrayList;
 
 public class LambdaList {
+    // Fungsi utama untuk menjalankan program
     public static void main(String[] args){
+        // Membuat list untuk menyimpan nama-nama mahasiswa
         ArrayList<String> mahasiswaList = new ArrayList<>();
+        
+        // Menambahkan elemen ke dalam list
         mahasiswaList.add("Adi");
         mahasiswaList.add("Bambang");
-        mahasiswaList.add("CIci");
+        mahasiswaList.add("Cici");
         mahasiswaList.add("Didi");
         
-        // Lambda sebagai parameter
+        // Menampilkan seluruh elemen list menggunakan ekspresi lambda
         mahasiswaList.forEach((nama) -> System.out.println(nama));
     }
 }
